d38b9866b3e276b6c26c27dfec3b8ce60abbd816
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-13 00:35:04 -0500

    playing with spok and Cypress

---
+++ cypress/integration/spec.js
@@ -0,0 +1,89 @@
+/// <reference types="Cypress" />
+
+import spok from "spok"
+import stripAnsi from "strip-ansi"
+
+spok.color = false
+spok.printDescription = false
+
+class Assert {
+  constructor() {
+    this.failed = []
+    this.passed = []
+  }
+
+  equal(actual, expected, msg) {
+    if (actual !== expected) {
+      this.failed.push(msg)
+    } else {
+      this.passed.push(msg)
+    }
+  }
+
+  deepEqual(actual, expected, msg) {
+    const pass = deepEqual(actual, expected)
+    if (!pass) {
+      this.failed.push(msg)
+    } else {
+      this.passed.push(msg)
+    }
+  }
+}
+
+// Cypress.Commands.overwrite('should', (should, ...args) => {
+//   if (args[1] === 'spok' && Object.keys(args[2].length > 1)) {
+//     // create individual should assertions
+//     // expect(args[0], 'spok', Cypress._.pick(args[2], 'name'))
+//     // expect(args[0], 'spok', Cypress._.pick(args[2], 'guess'))
+//     expect('foo').to.equal('foo')
+//     expect('bar').to.equal('bar')
+//   } else {
+//     return should(...args)
+//   }
+// })
+
+const spokAssertion = (_chai, utils) => {
+  function checkSpok(expectation) {
+    console.log('_chai is', _chai)
+    console.log('utils', utils)
+    console.log('value', this._obj)
+    console.log('expectations', expectation)
+
+    const assert = new Assert()
+    spok(assert, this._obj, expectation)
+
+    const agreement = assert.passed.map(stripAnsi).join(', ')
+    if (agreement) {
+      this.assert(true, agreement)
+    }
+
+    if (assert.failed.length) {
+      assert.failed.forEach((message) => {
+        this.assert(false, stripAnsi(message))
+      })
+    } else {
+      // assert.passed.forEach(message => {
+      //   this.assert(true, stripAnsi(message))
+      // })
+    }
+  }
+
+  _chai.Assertion.addMethod('spok', checkSpok)
+}
+
+chai.use(spokAssertion)
+
+it('spoks', () => {
+  const o = {
+    name: 'Hello world',
+    guess: 50
+  }
+  cy.wrap(o).should('spok', {
+    name: spok.startsWith('Hello'),
+    guess: spok.range(1, 10)
+  })
+
+  setTimeout(() => {
+    o.guess = 7
+  }, 1000)
+})

4e4014529c2e1c6c51c319f6dd555953fd89412a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-09 08:28:33 -0500

    feat: make compatible with esbuild

---
+++ cypress-esbuild.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+// https://github.com/bahmutov/cypress-esbuild-preprocessor
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+
+module.exports = defineConfig({
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', createBundler())
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})

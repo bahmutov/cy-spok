e4e05d17a802caf8f8324e4a29366ae10c380eaf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-08-28 16:30:27 -0400

    fix: remove invalid spok types

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,25 @@
-import type {
-  SpokAssertions,
-  SpokConfig,
-  Specifications,
-  SpokFunctionAny,
-  Assert
-} from 'spok/dist/types'
-import { ExpectFn } from 'spok/dist/adapter-chai-expect'
+// // import {
+//   SpokAssertions,
+//   SpokConfig,
+//   Specifications,
+//   SpokFunctionAny,
+//   Assert,
+// } from '@bahmutov/spok/dist/spok'
+// import { ExpectFn } from '@bahmutov/spok/dist/adapter-chai-expect'
 
-declare type SpokFunction<Subject> = (currentSubject: Subject) => void
+// declare type SpokFunction<Subject> = (currentSubject: Subject) => void
 
-declare type SpokHelper<Subject = any> = (
-  specifications: Specifications<Subject>
-) => SpokFunction<Subject>
+// declare type SpokHelper<Subject = any> = (
+//   specifications: Specifications<Subject>,
+// ) => SpokFunction<Subject>
 
-declare const Spok: SpokHelper &
-  SpokAssertions &
-  SpokConfig & {
-    any: SpokFunctionAny
-    adapters: {
-      chaiExpect: (expectFn: ExpectFn<any>) => Assert
-    }
-  }
+// declare const Spok: SpokHelper &
+//   SpokAssertions &
+//   SpokConfig & {
+//     any: SpokFunctionAny
+//     adapters: {
+//       chaiExpect: (expectFn: ExpectFn<any>) => Assert
+//     }
+//   }
 
-export = Spok
+// export = Spok
